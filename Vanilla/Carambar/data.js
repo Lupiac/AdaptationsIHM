4040c11a4cd7cd3f882e62bb4fd51a17a8a8ff1d
Alexis Deslandes <alexis.deslandes@etu.unice.fr>
2018-10-13 13:38:25 +0200

    Vanilla - computer and smartphone search version

---
+++ Vanilla/Carambar/data.js
@@ -0,0 +1,156 @@
+let articles_data = [
+    {
+        "name": "Fraise",
+        "id": 1,
+        "edition":"Edition normale",
+        "category":"Fruit",
+        "model":"Normal",
+        "image": "https://www.bonbonfactory.fr/wp-content/uploads/2016/01/carambar-fruits-300x300.jpg",
+        "weight": 25,
+        "desc": "Simple. Basique.",
+        "rating": 2,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.15,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.17,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.22,
+                "rating": 3.7
+            }
+        ]
+    },
+    {
+        "name": "Caramel",
+        "edition":"Edition normale",
+        "id": 2,
+        "model":"Normal",
+        "category":"Chocolaté",
+        "image": "https://www.bonbonfactory.fr/wp-content/uploads/2016/01/carambar-300x300.jpg",
+        "weight": 28,
+        "desc": "Un classique.",
+        "rating": 4,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.22,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.18,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.19,
+                "rating": 3.2
+            }
+        ]
+    },
+    {
+        "name": "Caranougat",
+        "edition":"Edition spéciale",
+        "id": 3,
+        "category":"Chocolaté",
+        "image": "https://www.bonbonfactory.fr/wp-content/uploads/2016/01/caranougat-300x300.jpg",
+        "weight": 27,
+        "model":"Normal",
+        "desc": "Succulent as fuck",
+        "rating": 5,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.25,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.32,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.27,
+                "rating": 3.2
+            }
+        ]
+    },
+    {
+        "name": "Titeuf",
+        "edition":"Edition exceptionelle",
+        "id": 4,
+        "model":"Normal",
+        "category":"Spéciaux",
+        "image": "http://static.flickr.com/101/288007680_806b149a0d.jpg",
+        "weight": 18,
+        "desc": "Edition limitée !",
+        "rating": 3,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.47,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.56,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.62,
+                "rating": 3.2
+            }
+        ]
+    },
+    {
+        "name": "Atomic",
+        "model":"Normal",
+        "edition":"Edition spéciale",
+        "category": "Spéciaux",
+        "id": 5,
+        "image": "https://www.espace-bonbon.fr/1268-large_default/carambar-atomic.jpg",
+        "weight": 27,
+        "desc": "Ce genre d'acidité",
+        "rating": 3,
+        "offers": []
+    },
+    {
+        "name": "Cola",
+        "model":"Normal",
+        "id": 6,
+        "edition":"Edition normale",
+        "category": "Spéciaux",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ7_WvCEgzIRqtukPBvaX8nhjkMXK-5mWBrhZneooesTJKavBb",
+        "weight": 27,
+        "desc": "C'est bon sa mère ça",
+        "rating": 5,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.32,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.41,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.29,
+                "rating": 3.2
+            }
+        ]
+    }
+
+
+];
