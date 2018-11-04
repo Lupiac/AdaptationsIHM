540a3bcf0bfd1893c12a7190d833641b0f4fc63c
unknown <jcr.clb@gmail.com>
2018-11-04 16:15:51 +0100

    version quasi finale

--- VueJS/data.js
+++ VueJS/data.js
@@ -1,138 +1,201 @@
 let articles_data = [
-{
-	"name": "Carambar Fraise",
-	"id":1,
-	"image": "https://www.bonbonfactory.fr/wp-content/uploads/2016/01/carambar-fruits-300x300.jpg",
-	"weight":25,
-	"desc":"Simple. Basique.",
-	"rating":2,
-	"offers": [
-	{
-		"user":"sebdu06",
-		"price":0.15,
-		"rating":4.3
-	},
-	{
-		"user":"philipZer",
-		"price":0.17,
-		"rating":4.7
-	},
-	{
-		"user":"alexiCGS",
-		"price":0.22,
-		"rating":3.7
-	}
-	]
-},
-{
-	"name": "Carambar Caramel",
-    "id":2,
-	"image": "https://www.bonbonfactory.fr/wp-content/uploads/2016/01/carambar-300x300.jpg",
-	"weight":28,
-	"desc":"Un classique.",
-	"rating":4,
-	"offers": [
-	{
-		"user":"sebdu06",
-		"price":0.22,
-		"rating":4.3
-	},
-	{
-		"user":"philipZer",
-		"price":0.18,
-		"rating":4.7
-	},
-	{
-		"user":"alexiCGS",
-		"price":0.19,
-		"rating":3.2
-	}
-	]
-},
-{
-	"name": "Caranougat",
-    "id":3,
-	"image": "https://www.bonbonfactory.fr/wp-content/uploads/2016/01/caranougat-300x300.jpg",
-	"weight":27,
-	"desc":"Succulent as fuck",
-	"rating":5,
-	"offers": [
-	{
-		"user":"sebdu06",
-		"price":0.25,
-		"rating":4.3
-	},
-	{
-		"user":"philipZer",
-		"price":0.32,
-		"rating":4.7
-	},
-	{
-		"user":"alexiCGS",
-		"price":0.27,
-		"rating":3.2
-	}
-	]
-},
-{
-	"name": "Carambar Titeuf",
-    "id":4,
-	"image": "http://static.flickr.com/101/288007680_806b149a0d.jpg",
-	"weight":18,
-	"desc":"Edition limitée !",
-	"rating":3,
-	"offers": [
-	{
-		"user":"sebdu06",
-		"price":0.47,
-		"rating":4.3
-	},
-	{
-		"user":"philipZer",
-		"price":0.56,
-		"rating":4.7
-	},
-	{
-		"user":"alexiCGS",
-		"price":0.62,
-		"rating":3.2
-	}
-	]
-},
-{
-	"name": "Carambar Atomic",
-    "id":5,
-	"image": "https://www.espace-bonbon.fr/1268-large_default/carambar-atomic.jpg",
-	"weight":27,
-	"desc":"Ce genre d'acidité",
-	"rating":3,
-	"offers": []
-},
-{
-	"name": "Carambar Cola",
-    "id":6,
-	"image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ7_WvCEgzIRqtukPBvaX8nhjkMXK-5mWBrhZneooesTJKavBb",
-	"weight":27,
-	"desc":"C'est bon sa mère ça",
-	"rating":5,
-	"offers": [
-	{
-		"user":"sebdu06",
-		"price":0.32,
-		"rating":4.3
-	},
-	{
-		"user":"philipZer",
-		"price":0.41,
-		"rating":4.7
-	},
-	{
-		"user":"alexiCGS",
-		"price":0.29,
-		"rating":3.2
-	}
-	]
-},
-
-
+    {
+        "name": "Carambar Fraise",
+        "id": 1,
+        "image": "https://www.bonbonfactory.fr/wp-content/uploads/2016/01/carambar-fruits-300x300.jpg",
+        "weight": 25,
+        "category": "fruit",
+        "size": "normal",
+        "desc": "Simple. Basique.",
+        "rating": 2,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.15,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.17,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.22,
+                "rating": 3.7
+            }
+        ]
+    },
+    {
+        "name": "Carambar Caramel",
+        "id": 2,
+        "image": "https://www.bonbonfactory.fr/wp-content/uploads/2016/01/carambar-300x300.jpg",
+        "weight": 28,
+        "category": "caramel",
+        "size": "normal",
+        "desc": "Un classique.",
+        "rating": 4,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.22,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.18,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.19,
+                "rating": 3.2
+            }
+        ]
+    },
+    {
+        "name": "Caranougat",
+        "id": 3,
+        "image": "https://www.bonbonfactory.fr/wp-content/uploads/2016/01/caranougat-300x300.jpg",
+        "weight": 27,
+        "desc": "Succulent as fuck",
+        "category": "caramel",
+        "size": "normal",
+        "rating": 5,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.25,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.32,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.27,
+                "rating": 3.2
+            }
+        ]
+    },
+    {
+        "name": "Carambar Titeuf",
+        "id": 4,
+        "image": "http://static.flickr.com/101/288007680_806b149a0d.jpg",
+        "weight": 18,
+        "category": "other",
+        "size": "normal",
+        "desc": "Edition limitée !",
+        "rating": 3,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.47,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.56,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.62,
+                "rating": 3.2
+            }
+        ]
+    },
+    {
+        "name": "Carambar Atomic",
+        "id": 5,
+        "image": "https://www.espace-bonbon.fr/1268-large_default/carambar-atomic.jpg",
+        "weight": 27,
+        "category": "other",
+        "size": "normal",
+        "desc": "Ce genre d'acidité",
+        "rating": 3,
+        "offers": []
+    },
+    {
+        "name": "Carambar Cola",
+        "id": 6,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ7_WvCEgzIRqtukPBvaX8nhjkMXK-5mWBrhZneooesTJKavBb",
+        "weight": 27,
+        "category": "other",
+        "size": "normal",
+        "desc": "C'est bon sa mère ça",
+        "rating": 5,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.32,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.41,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.29,
+                "rating": 3.2
+            }
+        ]
+    }, {
+        "name": "Mini Carambar Caramel",
+        "id": 7,
+        "image": "https://www.bonbonrama.com/1554-large/mini-carambar-caramel-vrac-1kg.jpg",
+        "weight": 17,
+        "category": "caramel",
+        "size": "mini",
+        "desc": "Du biff et du caramel",
+        "rating": 4,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.32,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.41,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.29,
+                "rating": 3.2
+            }
+        ]
+    },
+    {
+        "name": "Mini Carambars Mix",
+        "id": 8,
+        "image": "https://www.bonbonweb.com/10117/mini-carambar.jpg",
+        "weight": 17,
+        "category": "fruit",
+        "size": "mini",
+        "desc": "La taille ne compte pas",
+        "rating": 4,
+        "offers": [
+            {
+                "user": "sebdu06",
+                "price": 0.32,
+                "rating": 4.3
+            },
+            {
+                "user": "philipZer",
+                "price": 0.41,
+                "rating": 4.7
+            },
+            {
+                "user": "alexiCGS",
+                "price": 0.29,
+                "rating": 3.2
+            }
+        ]
+    },
 ];
